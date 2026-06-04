4b659790b37f368708e20d7f140355b4faf27dc4
“liuyuze-121” <13734807773@163.com>
2026-06-04 20:12:04 +0800

    修复了图索引部分的相关问题以及仓库链接错误问题

--- lab3_Pthread&Openmp_programming/src/pthread/pthread_flat.h
+++ lab3_Pthread&Openmp_programming/src/pthread/pthread_flat.h
@@ -3,7 +3,7 @@
 #include <queue>
 #include <vector>
 #include <cstdint>
-#include <arm_neon.h>
+#include "../simd/simd_search.h"
 
 struct FlatThreadData {
     float* base;
@@ -14,24 +14,6 @@ struct FlatThreadData {
     size_t k;
     std::priority_queue<std::pair<float, uint32_t>>* result;
 };
-
-static float inner_product_neon(const float* a, const float* b, size_t dim) {
-    float32x4_t sum0 = vmovq_n_f32(0.0f);
-    float32x4_t sum1 = vmovq_n_f32(0.0f);
-    size_t i = 0;
-    for (; i + 8 <= dim; i += 8) {
-        float32x4_t a0 = vld1q_f32(a + i);
-        float32x4_t a1 = vld1q_f32(a + i + 4);
-        float32x4_t b0 = vld1q_f32(b + i);
-        float32x4_t b1 = vld1q_f32(b + i + 4);
-        sum0 = vfmaq_f32(sum0, a0, b0);
-        sum1 = vfmaq_f32(sum1, a1, b1);
-    }
-    float extra = 0.0f;
-    for (; i < dim; i++) extra += a[i] * b[i];
-    sum0 = vaddq_f32(sum0, sum1);
-    return vaddvq_f32(sum0) + extra;
-}
 
 void* pthread_flat_worker(void* arg) {
     FlatThreadData* data = (FlatThreadData*)arg;
